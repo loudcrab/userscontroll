34e77bd70b3ea1cc0b0e6c810c804536fa12afcc
loudcrab <awdx94@mail.ru>
2020-11-02 17:33:58 +0300

    fix: remove dev tools

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.123
-        </p>
-        <Button>
-          234
-        </Button>
-        <Box bgcolor='red' display='flex'>
-          asd
-        </Box>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
